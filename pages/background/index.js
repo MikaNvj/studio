996c29187185312e254d8f819f8b2ff799ea2c94
Mikanvj <masivolamika@gmail.com>
2024-08-01 14:37:13 +0200

    e

--- pages/background/index.js
+++ pages/background/index.js
@@ -2,7 +2,7 @@ import styles from '../../styles/service.module.css';
 
 export default function Back() {
   return (
-    <div className={styles.service_section}>
+    <div className={styles.back_section}>
       
        
     </div>
