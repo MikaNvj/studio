27387153e8ee11a4a2ee1669f0cc0ead05c66d70
Mikanvj <masivolamika@gmail.com>
2024-08-09 16:39:18 +0200

    go

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,7 @@
 // pages/_document.js
 import { Html, Head, Main, NextScript } from 'next/document';
 
+
 export default function Document() {
   return (
     <Html lang="en">
@@ -9,7 +10,7 @@ export default function Document() {
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
 <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet"/>
       </Head>
-      <body>
+      <body style={{ margin: 0 }}>
         <Main />
         <NextScript />
       </body>
